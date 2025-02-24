5d8fcc2c01c9bc556a001d71423734fe95b48910
Luis Ruiz <ruizl1209@gmail.com>
2025-02-24 15:12:44 -0600

    Adding Button Composable

--- app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.composeandcoroutinesdemo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,7 +20,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { MainContent() }
+        setContent {
+            MainContent()
+        }
+    }
+
+    private fun onClickSampleButton() {
+        Toast.makeText(this, "Clicked button", Toast.LENGTH_SHORT).show()
     }
 }
 
@@ -29,6 +38,14 @@ fun MainContent() {
     ComposeAndCoroutinesDemoTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Text("Hola!!!", modifier = Modifier.padding(innerPadding))
+            SampleButton()
         }
+    }
+}
+
+@Composable
+fun SampleButton() {
+    Button({/* Do nothing for now */}) {
+        Text("Sample Button")
     }
 }
