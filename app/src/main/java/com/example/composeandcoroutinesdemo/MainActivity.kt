fcecb4e8c5255d816828554bf180e4e815b50253
Luis Ruiz <ruizl1209@gmail.com>
2025-02-24 15:35:41 -0600

    Removing function, adding extension function to Modifier

--- app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.composeandcoroutinesdemo
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeandcoroutinesdemo.ui.theme.ComposeAndCoroutinesDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +23,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             MainContent()
         }
-    }
-
-    private fun onClickSampleButton() {
-        Toast.makeText(this, "Clicked button", Toast.LENGTH_SHORT).show()
     }
 }
 
@@ -45,7 +41,12 @@ fun MainContent() {
 
 @Composable
 fun SampleButton() {
-    Button({/* Do nothing for now */}) {
-        Text("Sample Button")
+    Button({/* Do nothing for now */}, modifier = Modifier.getSampleButtonModifier()) {
+        Text("Button")
     }
+}
+
+private fun Modifier.getSampleButtonModifier(): Modifier {
+    return this
+        .width(200.dp)
 }
