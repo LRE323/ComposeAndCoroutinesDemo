a161d5423ddd1252232f0350a4063d0b68f2819b
Luis Ruiz <ruizl1209@gmail.com>
2025-02-24 14:05:39 -0600

    Removing comment

--- app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/example/composeandcoroutinesdemo/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeandcoroutinesdemo.ui.theme.ComposeAndCoroutinesDemoTheme
 
-class MainActivity : ComponentActivity() { // Test commit
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
